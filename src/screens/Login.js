8639b1e2e39720fce6645ad2f333628d59e1f52a
abhinavkumar03 <ak2711474@gmail.com>
2023-07-02 18:52:28 +0530

    api

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -10,8 +10,8 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // const response = await fetch("http://localhost:5000/api/loginuser",{
-        const response = await fetch("https://fastnfood.onrender.com/api/loginuser",{
+        const response = await fetch("http://localhost:5000/api/loginuser",{
+        // const response = await fetch("https://fastnfood.onrender.com/api/loginuser",{
             method:'POST',
             headers:{
                 "Accept":"*/*",
